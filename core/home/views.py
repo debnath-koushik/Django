03cd86efe2135d389d684dc76a2a70d3799c11d1
Koushik <koushik.eframe@gmail.com>
2025-08-12 11:14:06 +0530

    using django template inside home page

--- core/home/views.py
+++ core/home/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 # Create your views here.
 
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the home index.")
-    return render(request, 'home.html')
+    rand_num = random.randint(1, 100)
+    vegetables = ['carrot', 'broccoli', 'spinach', 'kale']
+    cities = [
+                {'name': 'New York', 'population': 8419600},
+                {'name': 'Los Angeles', 'population': 3980400},
+                {'name': 'Chicago', 'population': 2716000},
+                {'name': 'Houston', 'population': 2328000},
+                {'name': 'Phoenix', 'population': 1690000}
+            ]
+    context = {'rand_num': rand_num, 
+               'vegetables': vegetables, 
+               'cities': cities}
+    return render(request, 'home.html', context=context)
 
 def about(request):
     # return HttpResponse("This is the about page.")
